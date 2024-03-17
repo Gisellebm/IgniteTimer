3e4eb5b6c16137a6921779d75133a36955ef12bf
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2024-03-17 20:39:06 -0300

    Contexto no React - Contexto entre rotas - atualizações

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -14,6 +14,7 @@ interface Cycle {
   finishedDate?: Date
 }
 interface CyclesContextType {
+  cycles: Cycle[]
   activeCycle: Cycle | undefined
   activeCycleId: string | null
   amountSecondsPassed: number
@@ -84,6 +85,7 @@ export function CyclesContextProvider({
   return (
     <CyclesContext.Provider
       value={{
+        cycles,
         activeCycle,
         activeCycleId,
         markCurrentCycleAsFinished,
