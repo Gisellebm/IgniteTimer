bdb97cef73c868ad42dc6b58d1efc126fd876695
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2024-03-17 21:59:11 -0300

    Contexto no React - Contexto entre rotas - Reset do formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -36,7 +36,12 @@ export function Home() {
     },
   })
 
-  const { handleSubmit, watch } = newCycleForm
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task
@@ -51,7 +56,7 @@ export function Home() {
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(createNewCycle)}>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
